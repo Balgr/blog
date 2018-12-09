073b1793dfef2deb71cfce6d4dc39621951aa71a
Mehdi RAFENNE <mehdi.rafenne@etudiants.u-paris2.fr>
2018-12-09 03:11:56 +0100

    Table name is now defined directly in the model instead of being passed with each call to a method.

--- core/Model.php
+++ core/Model.php
@@ -11,6 +11,7 @@ namespace Core;
 abstract class Model
 {
     private $db;
+    private $tableName;
 
     /* Constantes d'exceptions */
 
@@ -23,16 +24,16 @@ abstract class Model
         } catch (\PDOException $e) {
             die($e->getMessage());
         }
+        $this->setTableName();
     }
 
     /**
-     * @param $tableName
      * @param $data
      * @return mixed
-     * Creates a new row in the $tableName table, with the provided $data.
+     * Creates a new row in the $this->tableName table, with the provided $data.
      * $data must contain the table columns names as its keys, and the required values as values.
      */
-    public function create($tableName, $data) {
+    public function create($data) {
         // Creates the list of rows and columns to add in the query (format : id, author_name, ...)
         $columnsInsert = implode(", ", array_keys($data));
         $rowInsert = "'". implode("', '", array_values($data)) ."'";
@@ -41,20 +42,19 @@ abstract class Model
         var_dump($columnsInsert);
         var_dump($rowInsert);
 
-        $req = "INSERT INTO " . $tableName . " (".$columnsInsert .") " . " VALUES (".$rowInsert.")";
+        $req = "INSERT INTO " . $this->tableName . " (".$columnsInsert .") " . " VALUES (".$rowInsert.")";
 
         return $this->db->query($req);
     }
 
     /**
-     * @param $id
      * @param $data
      * @return mixed
-     * Updates an existing row in the $tableName table, with the provided $data.
+     * Updates an existing row in the $this->tableName table, with the provided $data.
      * $data must contain the table columns names as its keys, and the required values as values.
      * $id will allow the function to update the correct row and MUST contain an 'id' key.
      */
-    public function update($tablename, $data) {
+    public function update($data) {
         // Creates the list of rows and columns to add in the query (format : id = value, author_name = value, ...)
         $values = '';
         foreach($data as $key => $val) {
@@ -65,51 +65,69 @@ abstract class Model
         $values = substr($values, 0, strlen($values)-2);
 
 
-        $req = "UPDATE ". $tablename . " SET " . $values . "WHERE id=" . $data['id'];
+        $req = "UPDATE ". $this->tableName . " SET " . $values . "WHERE id=" . $data['id'];
 
         return $this->db->query($req);
 
     }
 
     /**
-     * @param $tablename
      * @param $id
      * @return mixed
-     * Deletes the row with the provided $id in the $tablename table.
+     * Deletes the row with the provided $id in the $this->tableName table.
      *
      */
-    public function delete($tablename, $id) {
-        $req = "DELETE FROM " . $tablename . " WHERE id=" . $id;
+    public function delete($id) {
+        $req = "DELETE FROM " . $this->tableName . " WHERE id=" . $id;
         echo $req;
 
         return $this->db->query($req);
     }
 
     /**
-     * @param $tablename
      * @return mixed
-     * Returns the list of ALL rows in the $tablename table.
+     * Returns the list of ALL rows in the $this->tableName table.
      */
-    public function getAll($tablename) {
-        $req = "SELECT * FROM " . $tablename;
+    public function getAll() {
+        $req = "SELECT * FROM " . $this->tableName;
         $data = $this->db->query($req);
 
         return $data->fetchAll(\PDO::FETCH_CLASS);;
     }
 
     /**
-     * @param $tablename
      * @param $id
      * @return mixed
-     * Gets a single element from the table $tablename, with the required $id.
+     * Gets a single element from the table $this->tableName, with the required $id.
      */
-    public function getSingle($tablename, $id) {
+    public function getSingle($id) {
         if(is_integer($id)) {
-            $req = "SELECT * FROM " . $tablename . " WHERE id=" . (int) $id;
+            $req = "SELECT * FROM " . $this->tableName . " WHERE id=" . (int) $id;
             echo $req;
             $data = $this->db->query($req);
-            $data = $data->fetch(\PDO::FETCH_ASSOC);
-            return $data;
+            return $data->fetch(\PDO::FETCH_ASSOC);
         }
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTableName()
+    {
+        return $this->tableName;
+    }
+
+    /**
+     * @return mixed
+     *
+     * @throws \ReflectionException
+     * Sets the table name to the short
+     */
+    public function setTableName()
+    {
+        $class = new \ReflectionClass($this);
+        $class = $class->getShortName();
+        $this->tableName = strtolower(str_replace('Model', '', $class)) . 's';
+        //echo $this->tableName;
     }
 }
