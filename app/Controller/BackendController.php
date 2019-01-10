5d848bd4a95cdafd4ad160b0f3528bfcab3df998
Mehdi <mehdi.rafenne@gmail.com>
2019-01-10 20:06:37 +0100

    Used to show the backend Homepage

---
+++ app/Controller/BackendController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mehdi
+ * Date: 04/01/2019
+ * Time: 15:17
+ */
+
+namespace Blog\app\Controller;
+
+
+use Blog\app\Entity\Post;
+use Blog\app\Entity\User;
+use Blog\core\Controller;
+
+class BackendController extends Controller
+{
+    private $userController;
+    private $commentController;
+    private $postController;
+    private $currentUser;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->userController = new UserController();
+        $this->commentController = new CommentController();
+        $this->postController = new PostController();
+        if(!isset($_SESSION['user'])) {
+            throw new \Exception("Non connecté.");
+        }
+        else {
+            $this->currentUser = UserController::currentUser();
+            if(!$this->currentUser->isAdmin()) {
+                // REDIRECTION DROITS INSUFFISANTS
+                throw new \Exception("Droits insuffisants.");
+            }
+        }
+    }
+
+    public function showHomeAction() {
+        echo $this->twig->render("backend/home.html.twig", array("currentUser" => $this->currentUser));
+    }
+}
