6ecda8501a234af901cd3a9f30a2bd9fc89b36f9
Mehdi <mehdi.rafenne@gmail.com>
2019-01-16 22:05:06 +0100

    Redesign of pages 404 and 403

--- app/Controller/ErrorController.php
+++ app/Controller/ErrorController.php
@@ -12,16 +12,38 @@ use Blog\core\Controller;
 
 class ErrorController extends Controller
 {
+    private $degresChiffresTwig;
+
     public function __construct()
     {
         parent::__construct();
+        $this->degresChiffresTwig = [];
+        for($i = 0; $i < 3; $i++) {
+            $this->degresChiffresTwig[] = rand(-80, 80);
+        }
     }
 
     public function show404Action() {
-        echo $this->twig->render("404.html.twig");
+        echo $this->twig->render("404.html.twig", array('degres' => $this->degresChiffresTwig));
     }
 
     public function show403Action() {
-        echo $this->twig->render("403.html.twig");
+        echo $this->twig->render("403.html.twig", array('degres' => $this->degresChiffresTwig));
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDegresChiffresTwig()
+    {
+        return $this->degresChiffresTwig;
+    }
+
+    /**
+     * @param mixed $degresChiffresTwig
+     */
+    public function setDegresChiffresTwig($degresChiffresTwig)
+    {
+        $this->degresChiffresTwig = $degresChiffresTwig;
     }
 }
